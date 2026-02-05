56c6ecdcaf2b3b3d8af362c7514694fba5bc049d
Akhenova <daniel.lozano.dev717@gmail.com>
2026-02-05 00:48:54 +0000

    removed ireelevant things

--- scripts/nepher_view.py
+++ scripts/nepher_view.py
@@ -14,16 +14,8 @@ import sys
 from nepher.cli.commands import view as view_module
 
 if __name__ == "__main__":
-    # Enable debug mode to see full tracebacks
     os.environ["NEPHER_DEBUG"] = "1"
     
-    # Store original sys.argv so view command can extract AppLauncher args
-    # This must be done before Click processes the arguments
     view_module.original_argv = sys.argv.copy()
-    
-    # The view command now handles AppLauncher setup internally
-    # Just call it with the current sys.argv - it will extract
-    # nepher args and AppLauncher args appropriately
     from nepher.cli.commands.view import view
-    view()  # type: ignore[call-arg]
-
+    view()
